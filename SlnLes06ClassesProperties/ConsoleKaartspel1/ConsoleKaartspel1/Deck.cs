cd757db4fb44320d031bebb44c5a2ad15052f407
kurtis <kurtisitruk7@gmail.com>
2022-03-30 23:59:40 +0200

    try

---
+++ SlnLes06ClassesProperties/ConsoleKaartspel1/ConsoleKaartspel1/Deck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKaartspel1
+{
+    class Deck
+    {
+        private Kaart[] deck;
+        private int NeemKaart;
+        private const int Aantal_Kaarten = 52;
+        private Random rnd_nummer;
+
+
+
+        public Deck()
+        {
+
+            string[] Kleur = { "Harten", "Schoppen", "Ruiten", "Klaveren" };
+            deck = new Kaart[Aantal_Kaarten];
+            NeemKaart = 0;
+            rnd_nummer = new Random();
+
+            for (int count = 0; count < deck.Length; count++)
+                deck[count] = new Kaart(Nummer(ToString)[count % 11], Kleur[count / 13]);
+        }
+
+        public void Mengen()
+        {
+            NeemKaart = 0;
+
+            for (int first = 0; first < deck.Length; first++)
+            {
+                int second = rnd_nummer.Next(Aantal_Kaarten);
+                Kaart temp = deck[first];
+                deck[first] = deck[second];
+                deck[second] = temp;
+            }
+        }
+
+        public Kaart Dealkaart()
+        {
+            if (NeemKaart < deck.Length)
+                return deck[NeemKaart++];
+            else
+                return null;
+        }
+    }
+}
